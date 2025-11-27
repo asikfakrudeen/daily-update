aa0b7bbb78e074c557f128c8e5f14558f0e9a316
asikfakrudeen <mohamedasik.softsuave@gmail.com>
2025-11-27 15:50:34 +0530

    update

---
+++ update_array_in_state/src/App.jsx
@@ -0,0 +1,3 @@
+import './App.css'
+const App = () => <></>
+export default App
---
+++ update_array_of_objects/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import MyComponent from './MyComponent.jsx'
+
+createRoot(document.getElementById('root')).render(<MyComponent/>)
